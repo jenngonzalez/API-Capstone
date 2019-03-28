0fe5031016202bba49c9d4f8b66e5e9cdcfdd492
Jenn Gonzalez <jgonz9873@gmail.com>
2019-03-27 18:43:07 -0700

    some stylistic updates, fonts, colors

--- script.js
+++ script.js
@@ -58,13 +58,15 @@ function displayYouTubeResults(responseJson) {
   for (let i = 0; i < responseJson.items.length; i++){
     $('#youtube-results-list').append(
       `<li><h3>${responseJson.items[i].snippet.title}</h3>
-       <p>${responseJson.items[i].snippet.description}</p>
        <iframe src="https://www.youtube.com/embed/${responseJson.items[i].id.videoId}" width="320" height="240" controls allowFullScreen>
        </iframe>
        </li>`)};
   //display the results section  
   $('#youTubeResults').removeClass('hidden');
 };
+
+// removed description,   <p>${responseJson.items[i].snippet.description}</p>
+
 
 function displayEventResults(responseJson) {
     console.log(responseJson);
